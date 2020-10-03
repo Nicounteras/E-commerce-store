b7b4a4ba78b9ccfb00b21adfd349ec95d48e8a1c
Nicounteras <anicolas.contreras@stgeorges.edu.pe>
2020-10-02 20:25:45 -0500

    new push

--- jn/src/actions/productActions.js
+++ jn/src/actions/productActions.js
@@ -16,6 +16,21 @@ const listProducts = () => async (dispatch) =>{
 
 }
 
+const saveProduct = (product) = async(dispatch, getState) => {
+    try {
+        dispatch({type: PRODUCT_SAVE_REQUEST, payload: product})
+
+        const {userSignin:{userInfo}} = getState()
+
+        const { data } = await Axios.post("/api/products", product, {headers:{
+                "Authorization": "Bearer" + userInfo.token
+        }})
+        dispatch({type:PRODUCT_SAVE_SUCCESS, payload: data})
+    } catch (error) {
+        dispatch({type:PRODUCT_SAVE_FAIL, payload: error.message})
+    }
+}
+
 const detailsProduct = (productId) => async (dispatch) => {
     try{
         dispatch({type: PRODUCT_DETAILS_REQUEST, payload: productId})
@@ -27,4 +42,4 @@ const detailsProduct = (productId) => async (dispatch) => {
     }
 }
 
-export { listProducts, detailsProduct }
+export { listProducts, detailsProduct, saveProduct }
--- jn/src/screens/ProductsScreen.js
+++ jn/src/screens/ProductsScreen.js
@@ -10,54 +10,96 @@ function ProductsScreen (props){
 
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
+  const [image, setImage] = useState("")
+  const [brand, setBrand] = useState("")
+  const [numReviews, setNumReviews] = useState("")
+  const [category, setCategory] = useState("")
+  const [countInStock, setCountInStock] = useState("")
+  const [description, setDescription] = useState("")
+  const [rating, setRating] = useState("")
   const productSave = useSelector(state=> state.productSave)
-  const { loading, userInfo, error } = userSignin
+  const { loading:loadingSave , success: successSave, error: errorSave } = productSave
   const dispatch = useDispatch()
 
   useEffect(() => {
-   if(userInfo){
-     props.history.push("/")
-   }
+   
     return () => {
       //
     }
   }, [userInfo])
   const SubmitHandler = (e) => {
     e.preventDefault()
-    dispatch(singin(email, password))
+    dispatch(saveProduct(name, price, image, brand ,description, rating, category, countInStock, numReviews))
   }
 
   return <div className="form">
     <form onSubmit={SubmitHandler}>
       <ul className="form-container">
         <li>
-          <h2>Sign In</h2>
+          <h2>Create product</h2>
         </li>
         <li>
           {loading && <div>Loading .....</div>}
           {error && <div>Error</div>}
         </li>
         <li>
-          <label for="email">
-            Email
+          <label for="Name">
+            Name
           </label>
-          <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+          <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)}/>
         </li>
         <li>
-          <label for="password">
-            Password
+          <label for="Price">
+            Price
           </label>
-          <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+          <input type="text" name="Price" id="Price" onChange={(e) => setPrice(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Image">
+            Image
+          </label>
+          <input type="text" name="Image" id="Image" onChange={(e) => setImage(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Brand">
+            Brand
+          </label>
+          <input type="text" name="Brand" id="Brand" onChange={(e) => setBrand(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Category">
+            Category
+          </label>
+          <input type="text" name="Category" id="Category" onChange={(e) => setCategory(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Rating">
+            Rating
+          </label>
+          <input type="text" name="Rating" id="Rating" onChange={(e) => setRating(e.target.value)}/>
+        </li>
+        <li>
+          <label for="numReviews">
+             Num Reviews
+          </label>
+          <input type="number" name="numReviews" id="numReviews" onChange={(e) => setNumReviews(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Description">
+            Description
+          </label>
+          <textarea type="text" name="Description" id="Description" onChange={(e) => setDescription(e.target.value)}/>
+        </li>
+        <li>
+          <label for="Stock">
+            Stock
+          </label>
+          <input type="text" name="Stock" id="Stock" onChange={(e) => setCountInStock(e.target.value)}/>
         </li>
         <li>
           <button type="submit" className="button primary">
-            Sign In
+            Create
           </button>
-        </li>
-        <li>
-          <Link to="/register" className="button secondary text-center">
-          Create your Amazona account
-          </Link>
         </li>
       </ul>
     </form>
